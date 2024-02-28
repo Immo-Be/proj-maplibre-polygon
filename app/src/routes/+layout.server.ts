fda79d45034c3859cb30e4bd084736559978f9d1
immobe <i.beeck@gmail.com>
2024-02-28 19:28:29 +0100

    add console logs

--- app/src/routes/+layout.server.ts
+++ app/src/routes/+layout.server.ts
@@ -5,7 +5,6 @@ import PocketBase from 'pocketbase';
 import type { PageServerLoad } from './$types';
 
 const url = import.meta.env.VITE_PB_URL;
-console.log('🚀 ~ url: in layout.servrer', url);
 const db = new PocketBase(url);
 
 // export { error } from '@sveltejs/kit';
@@ -14,6 +13,8 @@ export const load: PageServerLoad = async () => {
 	const fetchVersions = async () => {
 		try {
 			console.log('fetching versions... from layout');
+			console.log('🚀 ~ url: in layout.servrer', url);
+			console.log('🚀 ~ db: in layout.servrer', db);
 			return await db.collection('plans').getFullList();
 		} catch (error) {
 			console.log('Something went wrong while fetching polygons: ', error);
